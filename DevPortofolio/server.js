0c49a6ccd294741d79ae5b5b5a996fe015bda248
enea250705 <eneamuja87@gmail.com>
2025-03-10 04:10:44 +0100

    Create server.js

---
+++ DevPortofolio/server.js
@@ -0,0 +1,23 @@
+
+const app = express();
+const path = require('path');
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Set the views directory to where your .ejs files are located
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files (CSS, JS, images) from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Example route to render the index.ejs file when someone visits the homepage
+app.get('/', (req, res) => {
+    res.render('index');  // This will look for 'views/index.ejs'
+});
+
+// Set up the server to listen on the specified port
+const port = process.env.PORT || 3000;  // Default to port 3000 if no environment variable is set
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
